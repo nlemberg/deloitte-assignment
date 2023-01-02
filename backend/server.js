4219a5d55374a395599ac344654b765ceb035617
nlemberg <naama.lemberg@gmail.com>
2023-01-02 23:37:28 +0200

    deploy prep #9

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 db.connectDB();
 db.populateDB();
@@ -15,16 +15,17 @@ app.use(cors());
 
 app.use("/api/employees", require("./routes/employeeRoutes"));
 
-if (process.env.NODE_ENV === "production" || "prod") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production" || "prod") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Not in production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Not in production"));
+// }
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 3000);
